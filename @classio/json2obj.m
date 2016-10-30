b23aa682c86f9814ddc6f25bf58497125dcdd307
John DeVitis <johndevitis@gmail.com>
2016-10-29 22:52:21 -0400

    updated json io for Table class compatibility

--- @classio/json2obj.m
+++ @classio/json2obj.m
@@ -1,12 +1,12 @@
 function json2obj(self,fname)
-%% json2obj
+%% json2obj(fullname)
 % 
 % wrapper function for jsonlab -> save/load json files. 
 %
 % does not load dependent properties 
 % 
 % inputs:
-%   fname -> full path/name to file
+%   fname -> full path/name.ext to file
 % 
 % author: john devitis
 % create date: 24-Oct-2016 15:22:29
@@ -17,6 +17,7 @@ function json2obj(self,fname)
         warning('please supply a source file (full file path)');
     end
     
+    % get class info
     cn = class(self);           % get class name
     names = fieldnames(self);   % get object field names
     
@@ -41,15 +42,34 @@ function json2obj(self,fname)
             return
         end
     end
-    
+     
     jnames = fieldnames(tmp.(jname));
     % loop to populate consistent struct names of root object
     fprintf(' ... loading object contents... ');
-    for ii = 1:length(names)
+    for ii = 1:length(names) 
         for jj = 1:length(jnames)
             % save ONLY if names match and property not dependent
             if strcmp(names{ii},jnames{jj}) && ~loadid(ii)
-                self.(names{ii}) = tmp.(jname).(names{ii});
+                
+                % -- Table check
+                if isa(self.(names{ii}),'Table') % target object is Table
+                    self.(names{ii}).name = tmp.(jname).(names{ii}).name;
+                    self.(names{ii}).fileName = tmp.(jname).(names{ii}).fileName;
+                    self.(names{ii}).data = tmp.(jname).(names{ii}).data;
+                    self.(names{ii}).colNames = tmp.(jname).(names{ii}).colNames;
+                    self.(names{ii}).rowNames = [tmp.(jname).(names{ii}).rowNames{:}]';
+%                     self.(names{ii}).levels = tmp.(jname).(names{ii}).levels;
+                    % BUG IN LEVELS RE NESTED CELL ARRAY - need to un-nest
+                        for ll = 1:length(tmp.(jname).(names{ii}).levels)
+                            if ~isempty(tmp.(jname).(names{ii}).levels{ll})
+                                self.(names{ii}).levels{ll} = [tmp.(jname).(names{ii}).levels{ll}{:}]'; 
+                            end
+                        end
+                        self.(names{ii}).levels = self.(names{ii}).levels';
+                else
+                    % original, do regular thang
+                    self.(names{ii}) = tmp.(jname).(names{ii});
+                end
             end
         end
     end
--- @classio/obj2json.m
+++ @classio/obj2json.m
@@ -1,5 +1,5 @@
 function obj2json(self,pth,name)
-%% save2json
+%% obj2json(pth,name)
 % 
 % wrapper function for jsonlab -> save/load json files
 %
@@ -17,16 +17,52 @@ function obj2json(self,pth,name)
     
     % check path
 	if nargin < 2
-        warning('please supply a destination for file');
+        error('please supply a destination for file');
     end
     
     % check filename
     if nargin < 3
+        warning('no filename supplied, using class name')
         name = class(self); % get class name
     end
     
+    % check for nestTable to avoid error converting custom Table class
+    if isa(self,'nestedTable')
+        chkTable = 1;
+        % need to convert Table to structure
+        names = fieldnames(self);
+        for ii = 1:length(names)
+            if isa(self.(names{ii}),'Table')
+                
+%                 % build struct array
+%                 %  loop entries/records
+%                 for kk = 1:self.(names{ii}).rowNumber
+%                     cpy.(names{ii})
+%                     
+%                 end
+                % table class, dump to structure
+                cpy.(names{ii}).name = self.(names{ii}).name;
+                cpy.(names{ii}).fileName = self.(names{ii}).fileName;
+                cpy.(names{ii}).data = self.(names{ii}).data;
+                cpy.(names{ii}).colNames = self.(names{ii}).colNames;
+                cpy.(names{ii}).rowNames = self.(names{ii}).rowNames;
+                cpy.(names{ii}).levels = self.(names{ii}).levels;
+                
+            else
+                % create copy
+                cpy.(names{ii}) = self.(names{ii});
+            end
+        end
+    else
+        chkTable = 0;
+    end
+    
     % format json string
-    str = savejson(name,self);
+    if chkTable == 0
+        str = savejson(name,self); % original
+    else
+        str = savejson(name,cpy);  % new Table class check
+    end
     
     % save to file - CAUTION - this will overwrite filename
     fprintf('Saving %s class... ',name);
